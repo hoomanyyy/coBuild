ce0883b3a40eab95432478a5e462b082d016e6f3
hooman <hoomankhodadadi91@gmail.com>
2026-08-10 06:53:52 -0700

    Update api.js

--- cobuild/src/api.js
+++ cobuild/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "http://localhost:5000";
+export const BASE_URL = "https://cobuild-rdkh.onrender.com";
 
 const api = axios.create({
   baseURL: BASE_URL,
