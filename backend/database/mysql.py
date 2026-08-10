8dfb9ecbd896ee2e9ec249461d3eeceeda3d711e
YourName <your-email@example.com>
2026-08-10 14:49:08 -0700

    new update

--- backend/database/mysql.py
+++ backend/database/mysql.py
@@ -1,12 +1,22 @@
 import mysql.connector
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASS")
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
 
 
 def get_connection():
     return mysql.connector.connect(
-        host="127.0.0.1",
-        user="root",
-        password="",
-        database="cobuild",
+        host=DB_HOST,
+        user=DB_USER,
+        password=DB_PASS,
+        database=DB_NAME,
         charset="utf8mb4"
     )
 
